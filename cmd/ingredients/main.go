03245df220832fc1938544a92b1270ef9ca951f4
Zack Scholl <zack.scholl@gmail.com>
2019-07-20 09:18:13 -0700

    add main

---
+++ cmd/ingredients/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	log "github.com/schollz/logger"
+	"github.com/schollz/ingredients"
+)
+
+func main() {
+	log.SetLevel("trace")
+	if len(os.Args) < 2 {
+		log.Error("need to have argument")
+		os.Exit(1)
+	}
+
+	b, _ := ioutil.ReadFile(os.Args[1])
+	ing, err := ingredients.ParseTextIngredients(string(b))
+	if err != nil {
+		log.Error(err)
+		os.Exit(1)
+	}
+	b, _ = json.MarshalIndent(ing, "", "    ")
+	fmt.Println(string(b))
+
+	// r, err := ingredients.NewFromFile(os.Args[1])
+	// if err != nil {
+	// 	log.Error(err)
+	// 	os.Exit(1)
+	// }
+	// err = r.Parse()
+	// if err != nil {
+	// 	log.Error(err)
+	// 	os.Exit(1)
+	// }
+
+	// b, _ := json.MarshalIndent(r.IngredientList().Ingredients, "", "    ")
+	// fmt.Println(string(b))
+}
